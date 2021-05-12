36b752ec504969a3cbb813870a2112ecbac967b6
Antonia Čičin-Ćapin <antonia.cicincapin@gmail.com>
2021-05-12 20:10:41 +0200

    kreiranje baze

--- skriptapp23.sql
+++ skriptapp23.sql
@@ -1,2 +1,24 @@
 drop database if exists firmapp23;
 create database firmapp23;
+ use firmapp23;
+ 
+create table projekt (
+sifra int not null primary key auto_increment,
+naziv varchar (50),
+cijena decimal (18,2)
+);
+
+create table programer (
+sifra int not null primary key auto_increment,
+ime varchar (50) not null,
+prezime varchar (50)not null,
+datumrođenja datetime,
+plaća decimal (18,2)
+);
+
+create table suđenje (
+projekt int,
+programer int,
+datumpočetka datetime,
+datumkraja datetime
+);
